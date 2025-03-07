bec9c1d45081d4d0d81b0c50d461ac3ecbf0f29c
javiiershada <javiiershada@gmail.com>
2025-03-07 17:42:17 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,5 @@
+// Generate a random Ts code here.
+
+let randomNumber = Math.random() * 10;
+let randomColor = `rgb(${Math.floor(randomNumber)}, ${Math.floor(randomNumber)}, ${Math.floor(randomNumber)})`;
+document.body.style.backgroundColor = randomColor;
