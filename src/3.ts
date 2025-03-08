78cd04d66cb455c2ae6b8e1de9f0166dae0eb178
javiiershada <javiiershada@gmail.com>
2025-03-08 20:50:02 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,5 @@
+// Code generated with love by Synthetix (https://www.synthetix.io).
+// Version 0.1.0 (12/31/2022)
+function getRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
